8ae497ae8d7249a97d2a34ff7898d8b12234dd27
fatih <fatihkarakus@hotmail.com>
2020-08-01 15:58:30 +0200

    Week 1

--- isolate/02-add-event-listener/exercises/write-the-handler.js
+++ isolate/02-add-event-listener/exercises/write-the-handler.js
@@ -1,25 +1,28 @@
-'use strict';
+"use strict";
 
 // fill in the _s
 
-const buttonEl = document.createElement('button');
-buttonEl.innerHTML = 'are you honest?';
+const buttonEl = document.createElement("button");
+buttonEl.innerHTML = "are you honest?";
 console.log(buttonEl.nodeName, buttonEl.cloneNode(true));
-
 
 const truthTestHandler = () => {
   // ask a user to confirm if they are honest
   // let them know what you think of their answer
-
+  const username = confirm("are you honest");
+  if (username === false) {
+    alert(`this is a paradox`);
+  } else {
+    alert(`thanks for being honest`);
+  }
 };
 
-buttonEl.addEventListener('click', truthTestHandler);
-
+buttonEl.addEventListener("click", truthTestHandler);
 
 // "click" the button once
-const clickEvent1 = new Event('click');
+const clickEvent1 = new Event("click");
 buttonEl.dispatchEvent(clickEvent1);
 
 // "click" the button again
-const clickEvent2 = new Event('click');
+const clickEvent2 = new Event("click");
 buttonEl.dispatchEvent(clickEvent2);
--- isolate/03-html-templates/exercises/write-the-template-2.js
+++ isolate/03-html-templates/exercises/write-the-template-2.js
@@ -1,33 +1,36 @@
-'use strict';
+"use strict";
 
 /**
- * renders an unordered list of all even numbers in an array
- * @param {number[]} numbers - an array of numbers
- * @param {string} id - a value to use for the list's id
- * @returns {string} an HTML <ul> string with all even numbers
+ * renders a summary/details element with a <p> in the body
+ * @param {string} caption - the summary
+ * @param {string} mainText - the full paragraph
+ * @returns {string} an HTML string
  */
 const renderSummaryDetails = (caption, mainText) => {
-  return (
-    `<details>\n` +
-    `  _\n` +
-    `  _\n` +
-    `</details>`
-  );
+  return `<details>\n  <summary>${caption}</summary>\n  <p>${mainText}</p>\n</details>`;
 };
 
+const _1_tallThings = renderSummaryDetails(
+  "tall things",
+  "trees, buildings, mountains."
+);
+console.assert(
+  _1_tallThings ===
+    "<details>\n  <summary>tall things</summary>\n  <p>trees, buildings, mountains.</p>\n</details>",
+  "Test 1: tall things"
+);
 
+const _2_shortThings = renderSummaryDetails(
+  "short things",
+  "ants, crumbs, sand."
+);
+console.assert(
+  _2_shortThings ===
+    "<details>\n  <summary>short things</summary>\n  <p>ants, crumbs, sand.</p>\n</details>",
+  "Test 2: short things"
+);
 
-const _1_tallThings = renderSummaryDetails('tall things', 'trees, buildings, mountains.');
-console.assert(_1_tallThings === '<details>\n  <summary>tall things</summary>\n  <p>trees, buildings, mountains.</p>\n</details>',
-  'Test 1: tall things');
-
-const _2_shortThings = renderSummaryDetails('short things', 'ants, crumbs, sand.');
-console.assert(_2_shortThings === '<details>\n  <summary>short things</summary>\n  <p>ants, crumbs, sand.</p>\n</details>',
-  'Test 2: short things');
-
-
-
-const divEl = document.createElement('div');
+const divEl = document.createElement("div");
 
 divEl.innerHTML = _1_tallThings;
 divEl.innerHTML += _2_shortThings;
