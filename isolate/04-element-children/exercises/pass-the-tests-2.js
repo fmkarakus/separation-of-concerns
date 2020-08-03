337c0cc31eca493051fb6b369955862056bf6c4e
fatih <fatihkarakus@hotmail.com>
2020-08-03 11:41:56 +0200

    update

--- isolate/04-element-children/exercises/pass-the-tests-2.js
+++ isolate/04-element-children/exercises/pass-the-tests-2.js
@@ -17,15 +17,12 @@ divEl.children[0].children[0].innerHTML = "to the top";
 
 console.log(divEl.nodeName, divEl.cloneNode(true));
 
-<<<<<<< HEAD
-console.assert(divEl.children[0].href === "#top", "Test: href");
-=======
-console.assert(divEl.children[0].getAttribute('href') === '#top',
-  'Test: href');
+console.assert(divEl.children[0].getAttribute("href") === "#top", "Test: href");
 
-console.assert(divEl.children[0].children[0].innerHTML === 'to the top',
-  'Test: button innerHTML');
->>>>>>> d5bfb27a971aebb0851b52c7459562827b139913
+console.assert(
+  divEl.children[0].children[0].innerHTML === "to the top",
+  "Test: button innerHTML"
+);
 
 console.assert(
   divEl.children[0].children[0].innerHTML === "to the top",
